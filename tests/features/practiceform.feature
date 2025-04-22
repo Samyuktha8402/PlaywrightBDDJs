abc5ab511a0064b08c26a09843032f62890b7ef1
Samyuktha8402 <chadasamyuktha8402@gmail.com>
2025-04-22 21:40:43 +0530

    test

--- tests/features/practiceform.feature
+++ tests/features/practiceform.feature
@@ -1,10 +1,11 @@
 Feature: Practice form Test
 Scenario: Verify Practice Form 
-    When I enter 'StudentFirstname' and 'StudentLastname' and 'Email'
+    When I enter "StudentFirstname1" and "StudentLastname1" and "tests.tests34@yopmail.com"
     Then I select Gender
-    Then I enter 'mobile number' and 'DOB' and 'Subjects'
+    Then I entered '7076383848' and '12/9/2009' 
+    # Then I click on language and enter "language"
     Then I click hobbies
-    Then I upload picture
+    Then I upload file 'testdata/XPATH.pdf'
     Then I enter 'CurrrentAddress'
-    Then I select state and dropdown and submut
+    Then I select state and dropdown and submit
     
--- tests/hooks/hooks.js
+++ tests/hooks/hooks.js
@@ -10,22 +10,22 @@ Before(async function () {
     this.page.goto('https://demoqa.com/automation-practice-form')
   });
 
-  AfterStep( async function ({result}) {
-    // This hook will be executed after all steps, and take a screenshot on step failure
-    if (result.status === Status.FAILED) {
-      const buffer = await this.page.screenshot();
-      await this.page.screenshot({ path: 'screenshots/screenshot1.png' });
-      this.attach(buffer.toString('base64'), 'base64:image/png');
-      console.log("Screenshot logged")
+//   AfterStep( async function ({result}) {
+//     // This hook will be executed after all steps, and take a screenshot on step failure
+//     if (result.status === Status.FAILED) {
+//       const buffer = await this.page.screenshot();
+//       await this.page.screenshot({ path: 'screenshots/screenshot1.png' });
+//       this.attach(buffer.toString('base64'), 'base64:image/png');
+//       console.log("Screenshot logged")
 
-    }
-  });
-After(async function () {
-    // Assuming this.driver is a selenium webdriver
-    console.log("i am last");
-    // await this.page?.close();
-    // await this.context?.close();
-    // await this.browser?.close();
+//     }
+//   });
+// After(async function () {
+//     // Assuming this.driver is a selenium webdriver
+//     console.log("i am last");
+//     await this.page?.close();
+//     await this.context?.close();
+//     await this.browser?.close();
     
     
-  });
+//   });
